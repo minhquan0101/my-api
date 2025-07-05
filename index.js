2e528d0226a51a484d281e0b6a4b8a497692fb3f
minhquan0101 <quan123587@gmail.com>
2025-07-05 10:46:24 +0700

    Thêm workflow CI/CD...... /

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 4031;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD 👋 q');
+  res.send('Hello CI/CD 👋 ');
 });
 
 app.listen(PORT, () => {
